2fb738659edbfd3879787a6202607edae57db25f
Aakash Mehta <aakash4916@gmail.com>
2024-04-13 00:23:08 +0530

    added text file and updated code to read the text file on providing -c option.

--- my-wc.cpp
+++ my-wc.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     cout << "You have entered " << argc << " arguments:"
     << "\n";
- 
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
+
+    for (int i = 0; i < argc; ++i){
+        if(strcmp(argv[i],"-c") == 0){
+
+            std::ifstream myfile;
+            myfile.open(argv[i+1]);
+            std::string myline;
+            if ( myfile.is_open() ) {
+            // while ( myfile ) { // equivalent to myfile.good()
+            std::getline (myfile, myline);
+            std::cout << myline << '\n';
+            // }
+            }
+            else {
+            std::cout << "Couldn't open file\n";
+            }
+        }
+    }
  
     return 0;
 }
