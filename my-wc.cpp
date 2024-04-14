3394bf8a5fa4a9724162b49dd80939206b4c5a3e
Aakash Mehta <aakash4916@gmail.com>
2024-04-15 01:21:27 +0530

    Added -l functionality to count number of lines in a text file.

--- my-wc.cpp
+++ my-wc.cpp
@@ -5,20 +5,22 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    cout << "You have entered " << argc << " arguments:"
-    << "\n";
-
+    if(argc == 1){
+        //help section
+    }
     for (int i = 0; i < argc; ++i){
-        if(strcmp(argv[i],"-c") == 0){
+        if(strcmp(argv[i],"-l") == 0){
 
             std::ifstream myfile;
             myfile.open(argv[i+1]);
             std::string myline;
+            int countOfLines = 0;
             if ( myfile.is_open() ) {
-            // while ( myfile ) { // equivalent to myfile.good()
-            std::getline (myfile, myline);
-            std::cout << myline << '\n';
-            // }
+                while ( myfile ) { // equivalent to myfile.good()
+                std::getline (myfile, myline);
+                countOfLines++;
+                }
+            cout << countOfLines-1 << " " << argv[i+1] << endl;
             }
             else {
             std::cout << "Couldn't open file\n";
